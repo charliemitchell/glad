f5f476d9aff9e80009295f6d5010cec70a4bdec4
charles mitchell <charliesemailis@gmail.com>
2016-07-23 08:49:50 -0700

    Fixing issue where testing environment flag was getting set after the config file was loaded

--- blueprint/src/glad.boot2test.js
+++ blueprint/src/glad.boot2test.js
@@ -1,5 +1,5 @@
+process.env.test = true;
 global.glad = require('glad');
 global.should = require('should');
 global.assert = require('assert');
-process.env.test = true;
 glad.server(function(app, express, server) {});
